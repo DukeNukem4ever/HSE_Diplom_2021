c3248ab28b11d4e19fab0267c18f8a1450cb2ac5
Artem Orekhov <31654733+DukeNukem4ever@users.noreply.github.com>
2021-05-21 08:01:08 -0700

    Add files via upload

--- correlation/error_counter.py
+++ correlation/error_counter.py
@@ -7,10 +7,7 @@ import json
 errors_1 = []
 errors_2 = []
 
-start_path = './Exam2014'
-for root, dirs, files in os.walk(start_path):
-
-    print('Где мы сейчас:', root)
+for root, dirs, files in os.walk('./Exam_2014_graph_anns'):
     for file in files:
         if file.endswith('.ann'):
             with open(os.path.join(root, file),'r',encoding='utf-8-sig') as opened:
@@ -20,14 +17,27 @@ for root, dirs, files in os.walk(start_path):
                 find_mistakes = re.findall(r"Agreement_errors|Confusion_of_structures|Word_order|Emphatic|Cleft|Interrogative|Standard|Attributes|Attr_participial|Parallel_construction|Negation|Comparative_", open_file)                
                 file_analyzed['num_of_errors'] = len(find_mistakes)
 
-                if file.endswith('_1.ann'):
-                    errors_1.append(file_analyzed)
-                else:
-                    errors_2.append(file_analyzed)
+                errors_1.append(file_analyzed)
+
+
+
+for root, dirs, files in os.walk('./Exam2014_opinion_ann'):
+    for file in files:
+        if file.endswith('.ann'):
+            with open(os.path.join(root, file),'r',encoding='utf-8-sig') as opened:
+                open_file = opened.read()
+                file_analyzed = {}
+                file_analyzed['name'] = re.sub('.ann','.txt',file)
+                find_mistakes = re.findall(r"Agreement_errors|Confusion_of_structures|Word_order|Emphatic|Cleft|Interrogative|Standard|Attributes|Attr_participial|Parallel_construction|Negation|Comparative_", open_file)                
+                file_analyzed['num_of_errors'] = len(find_mistakes)
+
+                errors_2.append(file_analyzed)
+
+
 
 errors_all = errors_1 + errors_2
 
-with open('error_table_synt_all.csv',
+with open('./error_table_synt_all.csv',
               'w') as f:
         keys = ['name',
                 'num_of_errors']
@@ -36,7 +46,7 @@ with open('error_table_synt_all.csv',
         w.writeheader()
         w.writerows(errors_all)
 
-with open('error_table_synt_graph.csv',
+with open('./error_table_synt_graph.csv',
               'w') as f:
         keys = ['name',
                 'num_of_errors']
@@ -45,7 +55,7 @@ with open('error_table_synt_graph.csv',
         w.writeheader()
         w.writerows(errors_1)
 
-with open('error_table_synt_opinion.csv',
+with open('./error_table_synt_opinion.csv',
               'w') as f:
         keys = ['name',
                 'num_of_errors']
